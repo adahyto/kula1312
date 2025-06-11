1938c0a968faf02152e78d4f32c731249a9bd2c4
Alex <adahy.to@gmail.com>
2025-06-11 07:39:50 +0200

    Update Details.svelte

--- app/src/Details.svelte
+++ app/src/Details.svelte
@@ -19,39 +19,40 @@
     </li>
     <li>
       Komunikując się z Kulą staraj się używać poniższych komend i nie
-      wprowadzać nowych.
+      wprowadzać nowych, które mają to samo znaczenie.
     </li>
     <li>
       Jak chcesz wyprowadzić ją na spacer, to zachowaj czujność kiedy widzisz
-      innego psa. Zdaża się jej być agresywną - czasami pomaga zapowiedzenie
-      drugiego psa zanim go obaczy ("A kto to?")
+      innego psa. Czasami jest agresywna. Zapowiedzenie
+      drugiego psa zanim go obaczy ("A kto to?") pomaga.
     </li>
   </ul>
 </section>
 <section>
   <h2>Komendy</h2>
   <p>
-    Kula zna kilka komend, które ułatwiają wspólną komunikację. Oto ich lista:
+    Kula zna kilka komend, które ułatwiają wspólną komunikację:
   </p>
   <ul>
+    <li>Idziemy</li>
     <li>Chodź/Chodź tu</li>
     <li>Tędy</li>
     <li>Wracaj tu</li>
-    <li>Zostań (chodzi o zamarznięcie w bezruchu)</li>
+    <li>Zostań</li>
     <li>Czekaj (swobodna wersja komendy wyżej)</li>
     <li>Hopa (odwołanie powyższych/zachęcenie do wskoczenia na łóżko)</li>
     <li>Super!</li>
-    <li>Już! (wzmocnienie innych komend)</li>
-    <li>Szybko (bieg/wzmocnienie innych komend)</li>
     <li>Nie!</li>
     <li>Fu!</li>
-    <li>Łapka/Daj łapkę</li>
-    <li>Druga (chodzi o podanie drugiej łapki)</li>
-    <li>Leżeć</li>
+    <li>Idź sobie</li>
+    <li>Już! (wzmocnienie innych komend)</li>
+    <li>Szybko (bieg/wzmocnienie innych komend)</li>
     <li>Śpij sobie śpij (to ją uspokaja)</li>
     <li>A kto to?! (jeśli chcesz, aby cieszyła się, że kogoś zobaczy)</li>
     <li>A co to jest dla Kuli?! (po tym wie, że dostanie jakiś przysmak)</li>
-    <li>Idź sobie</li>
+    <li>Łapka/Daj łapkę</li>
+    <li>Druga (chodzi o podanie drugiej łapki)</li>
+    <li>Leżeć</li>
     <li>Gdzie jest misio?! (szukanie zabawki)</li>
     <li>Dawaj brzucha (to przy głaskaniu, żeby położyła się na plecach)</li>
   </ul>
@@ -59,16 +60,16 @@
 <section>
   <h2>Jedzenie</h2>
   <p>
-    Jedzenie trzymam za szafką, na której są głośniki. Kula je 2 razy dziennie:
-    około 10 rano i 20 wieczorem, w międzyczasie dostaje jeden większy przysmak
-    i czasami mniejsze w zamian za sztuczkę lub uspokojenie się kiedy szczeka.
+    Kula je 2 razy dziennie:
+    około 8 rano i 19 wieczorem, w międzyczasie dostaje jeden większy przysmak
+    i czasami mniejsze w zamian za sztuczkę lub przy głaskaniu.
   </p>
 </section>
 <section>
   <h2>Spacery</h2>
   <p>
-    Najlepiej 3: przed jedzeniem rano, przed jedezeniem wieczorem i jakoś
-    pomiędzy. Jeśli nie ma opcji na 3, to 2: rano i wieczorem.
+    Najlepiej 3x: przed jedzeniem rano, przed jedezeniem wieczorem i jakoś
+    pomiędzy. Jeśli nie ma opcji na 3x, to 2x: rano i wieczorem.
   </p>
 </section>
 <section>
@@ -81,5 +82,3 @@
   </p>
 </section>
 
-<style>
-</style>
